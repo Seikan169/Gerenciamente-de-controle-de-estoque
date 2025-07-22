456d3d42c09030a98fe66e4d6f63d5571434ff42
Gabriel <Gabriel@GABRIEL>
2025-07-22 12:43:54 -0300

    Adicionar arquivos de projeto.

---
+++ Gerenciamente de controle de estoque/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerenciamente_de_controle_de_estoque
+{
+    internal class Produto
+    {
+        public string nome { get; set; }
+        public int quantidade { get; set; }
+        public decimal valor { get; set; }
+        public  string data_venc {  get; set; }
+        
+    }
+}
---
+++ Gerenciamente de controle de estoque/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Gerenciamente_de_controle_de_estoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public void MenuP()
+        {
+            Console.WriteLine("|=====================|");
+            Console.WriteLine("|         MENU        |");
+            Console.WriteLine("|=====================|");
+            Console.WriteLine("| 1 - Cadastrar       |");
+            Console.WriteLine("| 2 - Listar Produtos |");
+            Console.WriteLine("| 3 - Editar Produtos |");
+            Console.WriteLine("| 4 - Exluir Produtos |");
+            Console.WriteLine("| 5 |");
+        }
+    }
+}
