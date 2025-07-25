bb448211b142d78496fbef9642bf415ab37ce251
Gabriel Araújo <contato.bielgamer157@hotmail.com>
2025-07-25 14:32:49 -0300

    atualizar

--- Gerenciamente de controle de estoque/Program.cs
+++ Gerenciamente de controle de estoque/Program.cs
@@ -6,12 +6,11 @@ namespace Gerenciamente_de_controle_de_estoque
     {
         static void Main(string[] args)
         {
-            int opcao = 0;
+            int opcao = 0; // Variavel para navegar no menu 
             
-            string entradaquanti = "";
-            int quantidadeteste;
             do
             {
+                Console.Clear();
                 Produto produto1 = new Produto();
                 Console.WriteLine("|=====================|");
                 Console.WriteLine("|         MENU        |");
@@ -29,8 +28,8 @@ namespace Gerenciamente_de_controle_de_estoque
                 Console.WriteLine();
 
                 switch (opcao)
-                {
-                    case 1:
+                { // Começo do switch 
+                    case 1: // Inicio caso 1 
                         while (true)
                         {
                             Console.Clear();
@@ -53,32 +52,99 @@ namespace Gerenciamente_de_controle_de_estoque
                             Console.WriteLine();
 
 
-                            Inserindo(produto1);
+                            Inserindo(produto1); 
+
+                            Console.Write("Deseja Cadastrar mais  produtos ? (s/n) :");
+                            string comfirProd = Console.ReadLine().ToUpper();
+
+                            if (comfirProd == "S")
+                            {
+                                Console.WriteLine("Agurde...");
+                                Thread.Sleep(1000);
+                                
+                            }
+                            else if (comfirProd == "N")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Opção invalida. Por favor digite s ou n :");
+                                Console.ReadKey();
+                            }
+                            
 
                             
 
                         }
 
-                        break;
-                }
+                        break;// final caso 1 
+
+                    case 2: // INICIO CASO 2 
+                        Console.Clear();
+                        string campo = "";
+                        int opcaoBuscar = 0;
+                        string valorParaBusc = "";
+                        do
+                        {
+                            Console.WriteLine("|==================|");
+                            Console.WriteLine("|  Buscar Produtos |");
+                            Console.WriteLine("|==================|");
+                            Console.WriteLine("| 1 - Buscar todos |");
+                            Console.WriteLine("| 2 - ID           |");
+                            Console.WriteLine("| 3 - Nome         |");
+                            Console.WriteLine("| 4 - Vencimento   |");
+                            Console.WriteLine("| 5 - Voltar       |");
+                            Console.WriteLine("|==================|");
+                            Console.WriteLine();
+
+                            Console.Write("Escolha uma das opções acima (1 ao 4 ):");
+                            opcaoBuscar = int.Parse(Console.ReadLine());
+
+                            switch (opcaoBuscar)
+                            {
+
+                                case 1:
+                                    
+                                    valorParaBusc = "1";
+                                    Console.Clear();
+                                    BuscarProduto(opcaoBuscar,valorParaBusc);
+                                    Console.ReadKey();
+                                    
+
+                                    break;
+
+                                case 2:
+
+                                    break;
+                                default:
+                                    break;
+                            }
+
+
+
+
+                        } while (opcaoBuscar != 5);
+                        break; // FINAL CASO 2 
+                }// Final do swtich 
 
 
             } while (opcao != 5);
 
         }
         
-        static decimal lervalor(string mensagem)
+        static decimal lervalor(string mensagem) // Metodo para tratar o erro do valor 
         {
-            decimal testevalor;
+            decimal testevalor; 
             string entradavalor;
             while (true)
             {
-                Console.Write(mensagem);
-                entradavalor = Console.ReadLine().Trim();
+                Console.Write(mensagem);    // Recebendo os parametros 
+                entradavalor = Console.ReadLine().Trim(); // Tirando os espaços em brancos 
 
-                entradavalor = entradavalor.Replace(',', '.');
+                entradavalor = entradavalor.Replace(',', '.'); // Trocando as vírgulas por pontos 
 
-                if (decimal.TryParse(entradavalor, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out testevalor) && (testevalor >= 0))
+                if (decimal.TryParse(entradavalor, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out testevalor) && (testevalor >= 0)) // aceitando ponto e virgula com simbolo.
                 {
                     
                    return testevalor;
@@ -105,7 +171,7 @@ namespace Gerenciamente_de_controle_de_estoque
                 entradaquanti = Console.ReadLine();
                 Console.WriteLine();
 
-                if (int.TryParse(entradaquanti, out quantidadeteste) && (quantidadeteste >= 0))
+                if (int.TryParse(entradaquanti, out quantidadeteste) && (quantidadeteste >= 0)) // validando a quantidade se e numero e inteiro, e não pode ser menor que 0 
                 {
                     return quantidadeteste;
                     
@@ -126,12 +192,12 @@ namespace Gerenciamente_de_controle_de_estoque
             string entradadata;
             while (true)
             {
-                Console.Write(mensagem);
+                Console.Write(mensagem); // pegando os dados do usuario 
                 entradadata = Console.ReadLine();
 
                 if ((entradadata.Length == 8) && (entradadata.All(char.IsDigit)))
                 {
-                    entradadata = entradadata.Insert(2, "/").Insert(5, "/");
+                    entradadata = entradadata.Insert(2, "/").Insert(5, "/"); // Colocando a barra 
                     return DateTime.Parse(entradadata);
 
                 }
@@ -147,7 +213,7 @@ namespace Gerenciamente_de_controle_de_estoque
 
         static void Inserindo(Produto produto)
         {
-            string conexao = "server=localhost;user=root;password=SuaSenha.;database=Nomedobanco";
+            string conexao = "server=localhost;user=root;password=Suasenha.;database=nomedobanco";
 
             try
             {
@@ -168,9 +234,8 @@ namespace Gerenciamente_de_controle_de_estoque
 
                     Console.WriteLine("Produto inserido com sucesso!");
                     Console.WriteLine();
-                    Console.WriteLine("Aperte qualquer tecla para continuar !");
-                    Console.ReadKey();
-
+                    
+                    banco.Close();
 
                 }
             }
@@ -179,6 +244,72 @@ namespace Gerenciamente_de_controle_de_estoque
                 Console.WriteLine($"Error: {ex.Message}");
                 Console.ReadKey();
             }   
+        }
+
+        static void BuscarProduto(int opcao, string valor) 
+        {
+            string conexao = "server=localhost;user=root;password=suasenha@.;database=nomedobanco";
+            try
+            {
+                using (MySqlConnection banco = new MySqlConnection(conexao))
+                {
+                    banco.Open();
+                    string tabela = "";
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = banco;
+
+                    if (opcao == 1)
+                    {
+                        tabela = "SELECT * FROM Produto; ";
+                    }
+
+
+                    cmd.CommandText = tabela;
+
+                    using (MySqlDataReader tela = cmd.ExecuteReader())
+                    {
+                        bool achou = false;
+                        Console.WriteLine("|======================|");
+                        Console.WriteLine("| Produtos encontrados |");
+                        Console.WriteLine("|======================|");
+                        
+
+                        while (tela.Read())
+                        {
+                            achou = true;
+                           
+
+                            Console.WriteLine("ID :{0}", tela["id"]);
+                            Console.WriteLine("Nome :{0}", tela["Nome"]);
+                            Console.WriteLine("Quantidade :{0}", tela["Quantidade"]);
+                            Console.WriteLine("Valor :{0}", tela["valor"]);
+                            DateTime data = Convert.ToDateTime(tela["data_vencimento"]);
+                            Console.WriteLine("Data de Vencimento : {0:dd/MM/yyyy}", data); // Formatando a data
+                     
+                            Console.WriteLine("\n");
+
+                            Console.WriteLine();
+
+
+                        }
+                        
+                        if (!achou)
+                        {
+                            Console.WriteLine("Nenhum produto encontrado com esse critério.");
+                        }
+                            
+                            
+                    }
+
+
+
+                }
+            }
+            catch (Exception ex)  
+            {
+                Console.WriteLine("Erro! :{0}",ex.Message);
+            }
+
         }
 
 
